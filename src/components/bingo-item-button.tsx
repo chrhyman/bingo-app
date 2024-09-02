859e99702ed480db5ddaa4509bbfffd726470f48
Chris Hyman <chris@wugs.me>
2024-09-02 03:37:56 -0400

    bingo-app: 1.1 - adjust font size

--- src/components/bingo-item-button.tsx
+++ src/components/bingo-item-button.tsx
@@ -1,12 +1,11 @@
 import { Button, styled } from "@mui/material"
 
-const BingoItemButton = styled(Button)(({ theme }) => ({
+const BingoItemButton = styled(Button)({
   aspectRatio: 1 / 1,
   width: "100%",
   textOverflow: "ellipsis",
   textTransform: "none",
   lineHeight: 1,
-  fontSize: theme.typography.body1.fontSize,
-}))
+})
 
 export default BingoItemButton
